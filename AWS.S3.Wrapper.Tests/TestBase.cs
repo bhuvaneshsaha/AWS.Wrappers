3baf9426c7c07e4f39d62b7e48033f519f4d08e5
Bhuvanesh Sahadhevan <bhuvaneshmib@gmail.com>
2023-06-23 02:55:50 +0530

    Update test base to get AWS keys from secrets

--- AWS.S3.Wrapper.Tests/TestBase.cs
+++ AWS.S3.Wrapper.Tests/TestBase.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AWS.S3.Wrapper.Tests
+namespace AWS.S3.Wrapper.Tests;
+public class TestBase
 {
-    public class TestBase
-    {
-        protected static readonly IAmazonS3 client = new AmazonS3Client(
-            Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID"),
-            Environment.GetEnvironmentVariable("AWS_SECRET_ACCESS_KEY"),
-            Amazon.RegionEndpoint.USEast1
-        );
-    }
+    protected static readonly IAmazonS3 client = new AmazonS3Client(
+        Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID") ?? Environment.GetEnvironmentVariable("{{ secrets.AWS_ACCESS_KEY_ID }}"),
+        Environment.GetEnvironmentVariable("AWS_SECRET_ACCESS_KEY") ?? Environment.GetEnvironmentVariable("{{ secrets.AWS_SECRET_ACCESS_KEY }}"),
+        Amazon.RegionEndpoint.USEast1
+    );
 }
