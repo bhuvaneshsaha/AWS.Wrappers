d091c8017ad51535a0253884df80b20ea0411388
Bhuvanesh <bhuvaneshmib@gmail.com>
2024-01-24 12:42:05 +0530

    Improve Unit coverage for File Operations

--- AWS.S3.Wrapper.Tests/TestBase.cs
+++ AWS.S3.Wrapper.Tests/TestBase.cs
@@ -2,7 +2,11 @@
 namespace AWS.S3.Wrapper.Tests;
 public class TestBase
 {
-    protected readonly IAmazonS3 client = new AmazonS3Client(Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID"), Environment.GetEnvironmentVariable("AWS_SECRET_ACCESS_KEY"), Amazon.RegionEndpoint.USEast1);
+    protected readonly IAmazonS3 client = new
+        AmazonS3Client("AKIARPSFPGN4VSTKJP6U",
+"fx1sP5W+GLftaZI4nZMgoFYbH7Q2OqdlXre78eci", Amazon.RegionEndpoint.USEast1);
+
+
     protected readonly IBucketOperations _bucketOperations;
     protected readonly IDirectoryOperations _directoryOperations;
     protected readonly IFileOperations _fileOperations;
